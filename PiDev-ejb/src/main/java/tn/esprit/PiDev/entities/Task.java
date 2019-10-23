4c0edf335071d3088d51a1ea0f43e3e54c1d00be
LinaMeddeb <47324757+LinaMeddeb@users.noreply.github.com>
2019-10-23 23:51:45 +0100

    lina entites blech relations 2

--- PiDev-ejb/src/main/java/tn/esprit/PiDev/entities/Task.java
+++ PiDev-ejb/src/main/java/tn/esprit/PiDev/entities/Task.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,59 +19,53 @@ public class Task implements Serializable{
 	private int idTask ;
     private String title;
     private String description;
+	@Enumerated(EnumType.STRING)
+    private Status status;
     private static final long serialVersionUID = 1L;
     
-	//@OneToMany(mappedBy="employe")
-	//private List<WorkedOn> workedOn = new ArrayList<>() ;
-
-	public int getIdTask() {
-		return idTask;
-	}
-
-	public void setIdTask(int idTask) {
-		this.idTask = idTask;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	/*public List<WorkedOn> getWorkedOn() {
-		return workedOn;
-	}
-
-	public void setWorkedOn(List<WorkedOn> workedOn) {
-		this.workedOn = workedOn;
-	}
-*/
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-	public Task(int idTask, String title, String description) {
-		super();
-		this.idTask = idTask;
-		this.title = title;
-		this.description = description;
-	
-	}
-
 	public Task() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	public Task(int idTask, String title, String description, Status status) {
+		super();
+		this.idTask = idTask;
+		this.title = title;
+		this.description = description;
+		this.status = status;
+	}
+	public int getIdTask() {
+		return idTask;
+	}
+	public void setIdTask(int idTask) {
+		this.idTask = idTask;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+    
+	//@OneToMany(mappedBy="employe")
+	//private List<WorkedOn> workedOn = new ArrayList<>() ;
+
+
     
     
 }
